fe2da9001110d2d135873929b7ec21c3cdc52e33
Hemershon Silva <hemershon@gmail.com>
2023-02-10 20:24:04 -0300

    create and add gem rubocop

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -1,4 +1,3 @@
 class Backoffice::DashboardController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get 'dashboard/index'
   end
   devise_for :admins
-  root to: 'home#index'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
