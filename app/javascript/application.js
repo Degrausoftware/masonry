c86dc274a4e2f1e4972aad0b726bb1197ca666d8
Hemershon Silva <hemershon@gmail.com>
2023-02-11 19:16:47 -0300

    create and add gem rubocop

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'bulma/css/bulma.css'
+import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
