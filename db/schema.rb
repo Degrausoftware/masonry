2c0643f16c001bd9e7f0751a58a9aff6d1231e97
Hemershon Silva <hemershon@gmail.com>
2023-02-22 20:40:25 -0300

    Main (#24)
    * release/v0.1 (#21)
    
    * Feature/member (#19)
    
    * Readme
    
    * Readme
    
    * create model and test member
    
    * create controller and test member
    
    * create controller and test member
    
    * create controller and test member index
    
    * release/v0.1
    
    * chore: config rspec with factory_bot and shoulda_matchers
    
    * feat: create schema and migration of model son
    
    * feat: create model son
    
    * test: validation and factory model son
    
    * Update index member
    
    * Update index member
    
    * create member and test for controller and model
    
    * create member and test for controller and model
    
    ---------
    
    Co-authored-by: eskokado <eskokado@gmail.com>
    
    ---------
    
    Co-authored-by: eskokado <eskokado@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -12,39 +12,72 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_21_153456) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  create_table 'admins', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_admins_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_admins_on_reset_password_token', unique: true
   end
 
-  create_table "members", force: :cascade do |t|
-    t.string "city"
-    t.string "cpf"
-    t.string "degree_of_instruction"
-    t.string "email"
-    t.string "fathers_name"
-    t.string "mobile_phone"
-    t.string "mothers_name"
-    t.string "name"
-    t.string "nationality"
-    t.string "phone"
-    t.string "place_of_birth"
-    t.string "relationship"
-    t.string "state"
-    t.string "wedding_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'members', force: :cascade do |t|
+    t.string 'city'
+    t.string 'cpf'
+    t.string 'degree_of_instruction'
+    t.string 'email'
+    t.string 'fathers_name'
+    t.string 'mobile_phone'
+    t.string 'mothers_name'
+    t.string 'name'
+    t.string 'nationality'
+    t.string 'phone'
+    t.string 'place_of_birth'
+    t.string 'relationship'
+    t.string 'state'
+    t.date 'wedding_date'
+    t.date 'birth_date'
+    t.string 'blood_type'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  create_table "sons", force: :cascade do |t|
+    t.date "birth_date", null: false
+    t.string "gender", null: false
+    t.string "name_sons", null: false
+    t.bigint "member_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_sons_on_member_id"
+  end
+
+  add_foreign_key "sons", "members"
+  end
+
+  create_table 'members', force: :cascade do |t|
+    t.string 'city'
+    t.string 'cpf'
+    t.string 'degree_of_instruction'
+    t.string 'email'
+    t.string 'fathers_name'
+    t.string 'mobile_phone'
+    t.string 'mothers_name'
+    t.string 'name'
+    t.string 'nationality'
+    t.string 'phone'
+    t.string 'place_of_birth'
+    t.string 'relationship'
+    t.string 'state'
+    t.string 'wedding_date'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
   create_table "sons", force: :cascade do |t|
     t.date "birth_date", null: false
     t.string "gender", null: false
