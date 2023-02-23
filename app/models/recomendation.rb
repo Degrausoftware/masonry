130ee91f216605d53f42d74f85b85b01207bc815
eskokado <eskokado@gmail.com>
2023-02-23 17:16:59 -0300

    chore: documentation

--- app/models/recomendation.rb
+++ app/models/recomendation.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: recomendations
+#
+#  id           :bigint           not null, primary key
+#  father_mason :boolean          default(FALSE)
+#  mobile_phone :string
+#  name         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  member_id    :bigint           not null
+#
+# Indexes
+#
+#  index_recomendations_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => members.id)
+#
 class Recomendation < ApplicationRecord
   validates :name, :mobile_phone, :father_mason, presence: true
 
