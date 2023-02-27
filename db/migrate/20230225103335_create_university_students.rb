72248643cc463284cec78d4b9ddadaa596430881
Edson Shideki Kokado <eskokado@gmail.com>
2023-02-27 09:37:31 -0300

    Feature/create model university studante (#27)
    * feat: add university_students schema
    
    * feat: create university_students migration
    
    * feat: create university_students model
    
    * test: create university_students factory and validation

---
+++ db/migrate/20230225103335_create_university_students.rb
@@ -0,0 +1,12 @@
+class CreateUniversityStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :university_students do |t|
+      t.string :cim
+      t.string :mobile_phone
+      t.string :name
+      t.references :member, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
