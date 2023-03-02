ac2d527da10e601e1fd0e6d06c455e0fbccd7c3c
Edson Shideki Kokado <eskokado@gmail.com>
2023-03-02 17:28:00 -0300

    Feature/create model lowton (#32)
    * Readme
    
    * Readme
    
    * release/v0.1 (#21)
    
    * release/v0.1
    
    * Feature/member (#19)
    
    * Readme
    
    * Readme
    
    * create model and test member
    
    * create controller and test member
    
    * create controller and test member
    
    * create controller and test member index
    
    * release/v0.1
    
    * chore: config rspec with factory_bot and shoulda_matchers
    
    * feat: create schema and migration of model son
    
    * feat: create model son
    
    * test: validation and factory model son
    
    * Update index member
    
    * Update index member
    
    * create member and test for controller and model
    
    * create member and test for controller and model
    
    ---------
    
    Co-authored-by: eskokado <eskokado@gmail.com>
    
    * fix: comment
    
    * feat: create model Question into schema
    
    * feat: create model Question into migration
    
    * feat: create model Question
    
    * test: create model Question
    
    * feat: add model Lowton into Schema
    
    * feat: create migration of Model Lowton
    
    * feat: create Model Lowton
    
    * test: Model Lowton validation
    
    ---------
    
    Co-authored-by: Hemershon Silva <hemershon@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_203250) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_215332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_203250) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "lowtons", force: :cascade do |t|
+    t.string "cim"
+    t.string "mobile_phone"
+    t.string "name"
+    t.bigint "member_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_lowtons_on_member_id"
   end
 
   create_table "members", force: :cascade do |t|
@@ -102,6 +112,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_203250) do
     t.index ["member_id"], name: "index_voter_titles_on_member_id"
   end
 
+  add_foreign_key "lowtons", "members"
   add_foreign_key "questions", "members"
   add_foreign_key "recomendations", "members"
   add_foreign_key "sons", "members"
---
+++ spec/factories/lowtons.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: lowtons
+#
+#  id           :bigint           not null, primary key
+#  cim          :string
+#  mobile_phone :string
+#  name         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  member_id    :bigint           not null
+#
+# Indexes
+#
+#  index_lowtons_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => members.id)
+#
+FactoryBot.define do
+  factory :lowton do
+    cim { Faker::Number.number(digits: 8) }
+    mobile_phone { Faker::PhoneNumber.cell_phone_with_country_code }
+    name { Faker::Name.name }
+    member
+  end
+end
