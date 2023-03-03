a406c78bab08a2b6be4ad7997b4006b86af957f6
eskokado <eskokado@gmail.com>
2023-03-03 18:59:06 -0300

    feat: create migration model job

---
+++ db/migrate/20230303213805_create_jobs.rb
@@ -0,0 +1,22 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :address
+      t.string :carrer
+      t.string :city
+      t.string :current_position
+      t.date :employer
+      t.string :monthly_net_income
+      t.string :neihboarhood
+      t.string :phone
+      t.string :practice_another_profession
+      t.date :service_time
+      t.string :state
+      t.string :which
+      t.string :zip_code
+      t.references :member, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
