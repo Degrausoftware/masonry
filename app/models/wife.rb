ae704a63989f9c8fda4b0585b5b0ccbdf0e1d2e4
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-03-06 22:10:05 -0300

    feat: creating model wife

---
+++ app/models/wife.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: wives
+#
+#  id         :bigint           not null, primary key
+#  birth_date :date
+#  carrer     :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  member_id  :bigint           not null
+#
+# Indexes
+#
+#  index_wives_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => members.id)
+#
+class Wife < ApplicationRecord
+  validates :birth_date, :carrer, :name, presence: true
+
+  belongs_to :member
+end
