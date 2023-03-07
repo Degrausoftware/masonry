8c3b68cfc3177cb5edada9b57628056c38018a68
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-03-06 22:10:52 -0300

    test: creating test model wife

---
+++ spec/factories/wifes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :wife do
+    birth_date { 1000.days.from_now }
+    carrer { Faker::Job.title }
+    name { Faker::Name.name }
+    member
+  end
+end
---
+++ spec/models/wife_spec.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: wives
+#
+#  id         :bigint           not null, primary key
+#  birth_date :date
+#  carrer     :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  member_id  :bigint           not null
+#
+# Indexes
+#
+#  index_wives_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => members.id)
+#
+require 'rails_helper'
+
+
+RSpec.describe Wife, type: :model do
+  it 'is valid all attributes' do
+    wife = FactoryBot.create(:wife)
+    expect(wife).to be_valid
+  end
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:birth_date) }
+    it { is_expected.to validate_presence_of(:carrer) }
+    it { is_expected.to belong_to :member }
+  end
+end
