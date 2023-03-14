f117c6b29cef77f36289b1a03ef46779ddcb0eed
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-03-14 16:19:09 -0300

    test: model update Address

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id           :bigint           not null, primary key
+#  address      :string
+#  city         :string
+#  neighborhood :string
+#  state        :string
+#  zip_code     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  member_id    :bigint           not null
+#
+# Indexes
+#
+#  index_addresses_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => members.id)
+#
 FactoryBot.define do
   factory :address do
     address { Faker::Address.street_address }
