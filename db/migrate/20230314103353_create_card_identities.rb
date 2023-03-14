850fb3a2026877c99fdc10012db20fb76fb1723d
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-03-14 15:06:48 -0300

    feat: create migrate model-CardIdentity

---
+++ db/migrate/20230314103353_create_card_identities.rb
@@ -0,0 +1,12 @@
+class CreateCardIdentities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :card_identities do |t|
+      t.date :issue_date
+      t.string :issung_agency
+      t.string :number_identity
+      t.references :member, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
