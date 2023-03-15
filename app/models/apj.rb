15c93f6ce02714c1cbf04c411a82bb2e25875f6f
Caique Martins <caiquemartins.dev@gmail.com>
2023-03-15 15:10:18 -0300

    teste apj criado

--- app/models/apj.rb
+++ app/models/apj.rb
@@ -1,2 +1,5 @@
 class Apj < ApplicationRecord
+  validates :cim, :mobile_phone, :name,  presence: true
+
+  belongs_to :member
 end
--- spec/models/apj_spec.rb
+++ spec/models/apj_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Apj, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe apj, type: :model do
+  it 'is valid all attributes' do
+    apj = create(:apj)
+    expect(apj).to be_valid
+  end
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:cim) }
+    it { is_expected.to validate_presence_of(:mobile_phone) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to belong_to :member }
+  end
 end
