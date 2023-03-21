e57567e06d3e98a30e4e3a1a4ee54c16b1d9769c
Hemershon Silva <hemershon@gmail.com>
2023-03-21 11:13:30 -0300

    create apj and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_182603) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_140642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_182603) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "apjs", force: :cascade do |t|
+    t.string "cim"
+    t.string "mobile_phone"
+    t.string "name"
+    t.bigint "member_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_apjs_on_member_id"
   end
 
   create_table "card_identities", force: :cascade do |t|
@@ -239,6 +249,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_182603) do
   end
 
   add_foreign_key "addresses", "members"
+  add_foreign_key "apjs", "members"
   add_foreign_key "card_identities", "members"
   add_foreign_key "culture_religions", "members"
   add_foreign_key "demolays", "members"
