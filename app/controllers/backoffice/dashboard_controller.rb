aefc52e92c325b40772971d9ac827fc4e9fc33a8
Hemershon Silva <hemershon@gmail.com>
2023-08-05 22:13:30 -0300

    chore create son

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -13,9 +13,21 @@ module Backoffice
       @son = Son.count
     end
 
+    def show
+      find_masonic_lodge
+      binding.pry
+      @masonic_lodge = Masonic_lodge.members.find_by(masonic_lodge_id: params[:member_id])
+    end
+
     def destroy
       session[:admin_id] = nil
       redirect_to root_path, notice: 'Sai porra'
+    end
+
+    private
+
+    def find_masonic_lodge
+      @masonic_lodge = MasonicLodge.find(:id)
     end
   end
 end
--- app/controllers/backoffice/sons_controller.rb
+++ app/controllers/backoffice/sons_controller.rb
@@ -1,13 +1,53 @@
 # frozen_string_literal: true
 
 module Backoffice
-  class SonsController < ApplicationController
+  class SonsController < BackofficeController
+   before_action :set_masonic_lodge
+    #before_action :set_member
+
     def index
       @sons = Son.all
     end
 
     def show
-      @son = Son.find_by(:son_id)
+      @masonic_lodge = MasonicLodge.find_by(params[:masonic_lodge_id])
+      @member = @masonic_lodge.members.find(params[:member_id])
+      @son = @members.sons.find(params[:id])
+
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "Sobrinho não encontrado!"
+
+      redirect_to backoffice_masonic_lodge_member_son_path(masonic_lodge_id: params[:masonic_lodge_id], member_id: params[:member_id], id: params[:id])
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def set_membera
+      @member = Member.find_by(id: params[:member_id])
+    end
+
+    def set_masonic_lodge
+      @masonic_lodge = MasonicLodge.find params[:masonic_lodge_id]
+    end
+
+    def son_params
+      params.require(:son).permit(:id, :name_sons, :birth_date, :gender, :member_id, :image_son)
     end
   end
 end
